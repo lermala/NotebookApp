3eedc94df6989b9a00d52a2aa29bd32464671337
Lera Kolodina <kolodina.lera322@mail.ru>
2022-07-05 18:50:11 +0800

    Now we change style of each Note. Created style Pantone and empty Simple style

--- app/src/main/java/com/example/notebookapp/model/NotesService.kt
+++ app/src/main/java/com/example/notebookapp/model/NotesService.kt
@@ -1,13 +1,18 @@
 package com.example.notebookapp.model
 
 import com.example.notebook.model.Note
+import com.example.notebookapp.model.note_style.NoteStylePantone
 import com.github.javafaker.Faker
+import kotlin.random.Random
 
 class NotesService {
 
+    private val pantones = PantoneService().pantones
+
     val notes: ArrayList<Note> = (1..40).map { Note(
         title = Faker.instance().lorem().sentence(),
-        content = Faker.instance().lorem().paragraph(10)
+        content = Faker.instance().lorem().paragraph(10),
+        style = NoteStylePantone(pantoneColor = pantones[Random.nextInt(pantones.size)])
     )} as ArrayList<Note>
 
 }
---
+++ app/src/main/java/com/example/notebookapp/model/note_style/NoteStyleSimple.kt
@@ -0,0 +1,10 @@
+package com.example.notebookapp.model.note_style
+
+import com.example.notebookapp.R
+
+class NoteStyleSimple(
+
+): NoteStyle(KEY_STYLE_SIMPLE, R.layout.item_note_pantone){ // todo change layout
+
+
+}
